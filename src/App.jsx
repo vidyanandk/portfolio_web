4cda920260a139274a97755015ccbddbfbebe678
vidyanandk <vidyanandkumar200502@gmail.com>
2024-03-01 21:06:32 +0530

    technology section

--- src/App.jsx
+++ src/App.jsx
@@ -94,6 +94,67 @@ function App() {
                   </div>
             </div>
           </div>
+
+            {/* Technoglies section */}
+        <section className="py-10" id="technologies">
+          <div className="container m-auto px-4">
+            <h2 className="text-2xl font-semibold">Technologies</h2>
+            <div className="mt-14">
+              <div>
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">HTML</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">CSS, Sass & Bootstrap</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">
+                    JavaScript, TypeScript, JQuery
+                  </h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">UI design in Figma</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[75%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">Angular</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[50%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">React</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[90%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">React Native</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[45%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+            </div>
+          </div>
+        </section>
+        
         </section>
       </main>
     </>
