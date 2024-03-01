4a042a01ceedd9e7f5c0c4a9225b9ac09c863479
vidyanandk <vidyanandkumar200502@gmail.com>
2024-03-01 20:58:28 +0530

    Adding project section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@
 import './App.css'
 import Porfile from "./assets/Photo.jpg"
 // import ProfileBg from "./assets/small.png"
+import Project1 from "./assets/project1.png"
+import Project2 from "./assets/project2.png"
+import Project3 from "./assets/project3.png"
+import Project4 from "./assets/project4.png"
+
 function App() {
 
   return (
@@ -40,6 +45,53 @@ function App() {
             <div>
                   <img src={Porfile} width={300} height ={200} className='relative z-10' />
                   {/* <img src={ProfileBg} className='absolute top-0 left-12 z-0'/> */}
+            </div>
+          </div>
+        </section>
+        {/* Project section .. */}
+        <section>
+          <div className='container m-auto px-4'>
+            <h2 className='text-2xl font-semibold'>Projects</h2>
+              <div className='flex gap-10 mt-11'>
+                  <div className='border border-gray-500 rounded-md p-5'>
+                    <img src={Project1} />
+                    <h3 className='text-2xl font-semibold mt-8'>Lorem ipsum dolor sit.</h3>
+                    <p className='text-gray-400 text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium explicabo eius quibusdam aliquid.</p>
+                    <div className='flex justify-evenly mt-12'>
+                       <button className='px-6 py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700'>Live Preview</button>
+                       <button className='px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 '>Checkout github</button>
+                    </div>
+                  </div>
+                  <div className='border border-gray-500 rounded-md p-5'>
+                    <img src={Project2} />
+                    <h3 className='text-2xl font-semibold mt-8'>Lorem ipsum dolor sit.</h3>
+                    <p className='text-gray-400 text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium explicabo eius quibusdam aliquid.</p>
+                    <div className='flex justify-evenly mt-12'>
+                       <button className='px-6 py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700'>Live Preview</button>
+                       <button className='px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 '>Checkout github</button>
+                    </div>
+                  </div>
+            </div>
+
+            <div className='flex gap-10 mt-11'>
+                  <div className='border border-gray-500 rounded-md p-5'>
+                    <img src={Project3} />
+                    <h3 className='text-2xl font-semibold mt-8'>Lorem ipsum dolor sit.</h3>
+                    <p className='text-gray-400 text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium explicabo eius quibusdam aliquid.</p>
+                    <div className='flex justify-evenly mt-12'>
+                       <button className='px-6 py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700'>Live Preview</button>
+                       <button className='px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 '>Checkout github</button>
+                    </div>
+                  </div>
+                  <div className='border border-gray-500 rounded-md p-5'>
+                    <img src={Project4} />
+                    <h3 className='text-2xl font-semibold mt-8'>Lorem ipsum dolor sit.</h3>
+                    <p className='text-gray-400 text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium explicabo eius quibusdam aliquid.</p>
+                    <div className='flex justify-evenly mt-12'>
+                       <button className='px-6 py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700'>Live Preview</button>
+                       <button className='px-6 py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 '>Checkout github</button>
+                    </div>
+                  </div>
             </div>
           </div>
         </section>
